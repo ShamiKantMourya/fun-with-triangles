7893bd09a04499e276b2e36d35b6a79860f3d604
BlackxJoker <88420924+BlackxJoker@users.noreply.github.com>
2022-10-18 13:19:44 +0530

    docs: calculation part

--- isItTriangle.js
+++ isItTriangle.js
@@ -2,10 +2,20 @@ const angleOfTriangle = document.querySelectorAll(".angle");
 const checkButon = document.querySelector("#btn");
 const outputArea = document.querySelector(".output");
 
-
+function checkSumOfAngles(angle1 , angle2 , angle3) {
+    const sumOfAngles = angle1 + angle2 + angle3
+    return sumOfAngles;
+}
 
 function isItTriangle(){
-    
+ let sumOfAngles = checkSumOfAngles(Number(angleOfTriangle[0].value), Number(angleOfTriangle[1].value), Number(angleOfTriangle[2].value));
+
+ if (sumOfAngles===180) {
+    console.log("yay..! This is a Triangle")
+ }
+ else {
+    console.log("It's not a Triangle")
+ }
 }
 
 

c5d4275661b16e827c1989ef1ae35d2103a8c764
BlackxJoker <88420924+BlackxJoker@users.noreply.github.com>
2022-10-21 11:35:57 +0530

    javascript calculation part

--- quiz.js
+++ quiz.js
@@ -2,23 +2,19 @@ const quizForm = document.querySelector(".quiz");
 const submitButton = document.querySelector("#btn");
 const outputArea = document.querySelector("#output");
 
-const quizAnswers = ["90°","right angle","one right angle","85°","45°"];
+const quizAnswers = ["90°", "right angle" , "one right angle" , "85°" , "45°"];
 
 
 function totalScore(){
     let score = 0;
     let index = 0;
     const formResult =  new FormData(quizForm);
-    for (const value of formResult.values) {
-        if (value===quizAnswers[index]) {
-            score = score+1;
+    for (let value of formResult.values()) {
+        if (value === quizAnswers[index]) {
+            score = score + 1;
         }
-        index+1;
+       index = index + 1;
     }
+    outputArea.innerText = "Your total score is : " + score;
 }
-outputArea.innerText = "Your total score is : " + score;
-
-
-
-
-submitButton.addEventListener('click', quizResult)
+submitButton.addEventListener('click', totalScore);

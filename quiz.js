061a2891d0a323bb8e7483c8a7714f82d1a5c4a3
BlackxJoker <88420924+BlackxJoker@users.noreply.github.com>
2022-10-21 10:59:20 +0530

    docs: all tag and button call

---
+++ quiz.js
@@ -0,0 +1,16 @@
+const quizForm = document.querySelector(".quiz");
+const submitButton = document.querySelector("#btn");
+const outputArea = document.querySelector("#output");
+
+const quizAnswers = ["90°","right angle","one right angle","85°","45°"];
+
+
+function quizResult(){
+    let score = 0;
+    let index = 0;
+}
+
+
+
+
+submitButton.addEventListener('click', quizResult)

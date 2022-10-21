cfe326191289159b34734129d58be26c71ece7e0
BlackxJoker <88420924+BlackxJoker@users.noreply.github.com>
2022-10-21 11:14:02 +0530

    docs: output area for result

--- quiz.js
+++ quiz.js
@@ -5,10 +5,18 @@ const outputArea = document.querySelector("#output");
 const quizAnswers = ["90°","right angle","one right angle","85°","45°"];
 
 
-function quizResult(){
+function totalScore(){
     let score = 0;
     let index = 0;
+    const formResult =  new FormData(quizForm);
+    for (const value of formResult.values) {
+        if (value===quizAnswers[index]) {
+            score = score+1;
+        }
+        index+1;
+    }
 }
+outputArea.innerText = "Your total score is : " + score;
 
 
 
